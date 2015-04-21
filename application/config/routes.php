0df35ede9ee13bf1086be1c10062bd3c5fa5379e
Kate Lee <hslee.33754@gmail.com>
2015-04-21 16:52:39 -0700

    Adds create capability to news

--- application/config/routes.php
+++ application/config/routes.php
@@ -57,6 +57,7 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 */
 
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
